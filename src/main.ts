80737fbe88cc940cb522c696fa3538dda65043bf
SouthernPenguin <940583551@qq.com>
2024-07-29 10:54:23 +0800

    test(custom): 测试eslint自动修复

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
 import './style.css';
 import App from './App.vue';
-
-createApp(App)
-.use(ElementPlus)
-.use(createPinia())
-.mount('#app');
+createApp(App).use(ElementPlus).use(createPinia()).mount('#app');
+const a = 's';
